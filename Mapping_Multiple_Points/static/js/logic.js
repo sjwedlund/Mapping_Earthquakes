01a8cc73214467514f908723d0a33ce77b4fa4ef
Spring Wedlund <wedlundspring@gmail.com>
2021-09-24 14:06:18 -0700

    updating Mapping_Single_Points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -11,7 +11,8 @@ let cityData = cities;
 cityData.forEach(function(city) {
     console.log(city)
     L.circleMarker(city.location, {
-        radius: city.population/100000
+        radius: city.population/100000,
+        color: 'orange'
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map);
